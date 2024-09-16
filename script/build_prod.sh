0659eca75bb6bceddc8083320dd5c3479e3b245f
Rolando Medina Rosner <rolmedro@gmail.com>
2024-09-16 19:08:38 -0400

    feat: added build scripts

---
+++ script/build_prod.sh
@@ -0,0 +1,5 @@
+npm --prefix ./frontend_react/ install
+npm --prefix ./frontend_react/ audit fix --force
+npm --prefix ./frontend_react/ run build
+
+/home/rolando/anaconda3/envs/dyrtransportes_flask/bin/python backend_flask/src/app.pynpm --prefix ./frontend_react/ run dev
