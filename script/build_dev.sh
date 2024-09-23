f65b290c21ba8e832c6bfb8e53e57666ac115606
Rolando Medina Rosner <rolmedro@gmail.com>
2024-09-22 21:31:55 -0400

    update: subprojects refactor

--- script/build_dev.sh
+++ script/build_dev.sh
@@ -1,5 +1,5 @@
+/home/rolando/anaconda3/envs/dyrtransportes_flask/bin/python backend_flask/src/app.py
+
 npm --prefix ./frontend_react/ install
 npm --prefix ./frontend_react/ audit fix --force
 npm --prefix ./frontend_react/ run dev
-
-/home/rolando/anaconda3/envs/dyrtransportes_flask/bin/python backend_flask/src/app.pynpm --prefix ./frontend_react/ run dev
--- script/build_prod.sh
+++ script/build_prod.sh
@@ -2,4 +2,4 @@ npm --prefix ./frontend_react/ install
 npm --prefix ./frontend_react/ audit fix --force
 npm --prefix ./frontend_react/ run build
 
-/home/rolando/anaconda3/envs/dyrtransportes_flask/bin/python backend_flask/src/app.pynpm --prefix ./frontend_react/ run dev
+/home/rolando/anaconda3/envs/dyrtransportes_flask/bin/python backend_flask/src/app.py
